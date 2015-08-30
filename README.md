fa95101f6c7b2df328cdef92a511d1531d63a10f
Shun Takebayashi <shun@takebayashi.asia>
2015-08-30 13:05:59 +0900

    Create project

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WWW::YahooJapan::Baseball - It's new $module
+
+# SYNOPSIS
+
+    use WWW::YahooJapan::Baseball;
+
+# DESCRIPTION
+
+WWW::YahooJapan::Baseball is ...
+
+# LICENSE
+
+Copyright (C) Shun Takebayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Shun Takebayashi <shun@takebayashi.asia>
---
+++ lib/WWW/YahooJapan/Baseball.pm
@@ -0,0 +1,39 @@
+package WWW::YahooJapan::Baseball;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::YahooJapan::Baseball - It's new $module
+
+=head1 SYNOPSIS
+
+    use WWW::YahooJapan::Baseball;
+
+=head1 DESCRIPTION
+
+WWW::YahooJapan::Baseball is ...
+
+=head1 LICENSE
+
+Copyright (C) Shun Takebayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Shun Takebayashi E<lt>shun@takebayashi.asiaE<gt>
+
+=cut
+
